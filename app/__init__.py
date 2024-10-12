95ca6e0c0f740c1eeaa8fba19617677c959d3e15
Max <127511368+maximilian-hack@users.noreply.github.com>
2024-10-12 13:08:44 +0200

    this works i guess...

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os  # Import the os module for generating a random key
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Set a secret key for the session management
+app.secret_key = os.urandom(24)  # Generates a random key
 
 db = SQLAlchemy(app)
 
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,27 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from app import app, db
 from app.models import URL
 
-@app.route('/create', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        short_url_input = request.form['short_url']
+        url = URL.query.filter_by(short_url=short_url_input).first()
+        if url:
+            return render_template('index.html', original_url=url.original_url, short_url=short_url_input)
+        else:
+            flash('Short URL not found. Please try again.', 'error')
+    return render_template('index.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
     if request.method == 'POST':
         original_url = request.form['url']
         new_url = URL(original_url)
         db.session.add(new_url)
         db.session.commit()
-        return render_template('index.html', short_url=new_url.short_url)
-    return render_template('index.html')
+        return render_template('create.html', short_url=new_url.short_url)
+    return render_template('create.html')
 
 @app.route('/<short_url>')
 def redirect_url(short_url):
